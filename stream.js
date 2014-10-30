28d5148a29669e6f75c2ce794b5fb0b9c634c13c
godzillad <arunava82@gmail.com>
2014-10-30 18:52:36 +0530

    initial commit

---
+++ stream.js
@@ -0,0 +1,44 @@
+function Stream(start,step,filterFunc){
+
+	this.head = start;
+	if(step)
+		this.step = step;
+	else 
+		this.step = 1;
+	this.nextVFunc = function (n) { return n + this.step;}
+	if (filterFunc)
+		this.filterFunc = filterFunc;
+	else
+		this.filterFunc = null;
+		
+	this.tail = function () {
+		return new Stream( this.nextVFunc (this.head ), this.step, this.filterFunc);
+	};
+	
+	this.take = function (n){
+		if (n == 0)
+			return [];
+		if(this.filterFunc){
+			var h = this.filterFunc(this.head);
+			if (h)
+				return [this.head].concat(this.tail().take(n - 1)); 
+			else
+				return this.tail().take(n); 
+		}else{
+			return [this.head].concat(this.tail().take(n - 1));
+		}
+	}
+	
+	this.cons  = function (tailStream){
+		var f = function () { return tailStream; }
+		this.tail = f;
+		return this;
+	}
+	
+	this.filter = function (f){
+		return new Stream(this.head, this.step, f);
+	}
+}
+
+
+
